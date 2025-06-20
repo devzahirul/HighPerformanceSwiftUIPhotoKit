50e039105e1d1df43c392cc23f671e1047090a75
zahirulAIIUB <jislam150@gmail.com>
2025-06-20 14:40:01 +0600

    Initial Commit

---
+++ PhotoSwiftUISmoothly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoSwiftUISmoothly
+//
+//  Created by lynkto_1 on 6/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoSwiftUISmoothly/PhotoSwiftUISmoothlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwiftUISmoothlyApp.swift
+//  PhotoSwiftUISmoothly
+//
+//  Created by lynkto_1 on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwiftUISmoothlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
